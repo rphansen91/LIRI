ec80a68869a881f3344878b6e83afd4fad32f225
Ryan Hansen <rphansen91@gmail.com>
2017-07-20 21:49:53 -0700

    twitter keys

--- keys.js
+++ keys.js
@@ -1,11 +1,11 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
-  consumer_key: 'nvtRalD2wNvPG8woJeak9dePl',
-  consumer_secret: 'mxfscX7Rwn6nDogRjVCofGaix3yXyqMEmNqrGAQmxqVHCHry7E',
+    consumer_key: 'nvtRalD2wNvPG8woJeak9dePl',
+    consumer_secret: 'mxfscX7Rwn6nDogRjVCofGaix3yXyqMEmNqrGAQmxqVHCHry7E',
+    access_token_key: '203801467-5h6fTuaoZPDJSIsTe9T3WfLUNHajyCRUbJicqxZs',
+    access_token_secret: 'UXf4vw1bcwaKyDyx54SDcRL77N1i8PgrI6w8iyksgAENj',
 }
 
 exports.spotifyKeys = {
-  access_token_key: '2a35e7ba281f4c21bd7265a291306afd',
-  access_token_secret: '8504ab99da754c33b7c2054a6de7c25e',
+    access_token_key: '2a35e7ba281f4c21bd7265a291306afd',
+    access_token_secret: '8504ab99da754c33b7c2054a6de7c25e',
 }
