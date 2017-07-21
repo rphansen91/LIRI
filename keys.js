8434a6dfb139a7dfe0833609c60b938521e931b7
Giuseppe Conoscenti <jjconoscenti@Giuseppes-MacBook-Pro.local>
2017-07-20 20:12:44 -0500

    new files

--- keys.js
+++ keys.js
@@ -1,7 +1,8 @@
-var twitterKey = {
-    key: nvtRalD2wNvPG8woJeak9dePl,
-    appOnlyAuthentication: "https://api.twitter.com/oauth2/token",
-    requestTokenURL: "https://api.twitter.com/oauth/request_token",
-    authorizeURL: "https://api.twitter.com/oauth/authorize",
-    accessTokenURL: "https://api.twitter.com/oauth/access_token"
-};
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'nvtRalD2wNvPG8woJeak9dePl',
+    consumer_secret: 'mxfscX7Rwn6nDogRjVCofGaix3yXyqMEmNqrGAQmxqVHCHry7E',
+    access_token_key: '203801467-5h6fTuaoZPDJSIsTe9T3WfLUNHajyCRUbJicqxZs',
+    access_token_secret: 'UXf4vw1bcwaKyDyx54SDcRL77N1i8PgrI6w8iyksgAENj',
+}
